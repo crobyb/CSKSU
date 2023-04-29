c3460c2a7e6af4bdd82c5d0c3c3abcc7d42fa225
Nicholas Bear <99135011+NicholasBear97@users.noreply.github.com>
2023-04-29 18:33:02 -0400

    Connection to DB refactored
    Change the call to function that will send DB data. Reroute function to check movies.json file for movie data.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 //configure data label, type, and value
-function abstract_action(element) {
+function data_director(element) {
     var dataLabel = element.id;
-    var dataObjects = document.getElementById(dataLabel).value;
-    var data_container = { dataObjects }
+    var data = document.getElementById(dataLabel).value;
+    var data_container = { data };
     var converter = JSON.stringify(data_container);
     data_bus(dataLabel, converter);
 }
@@ -10,21 +10,34 @@ function abstract_action(element) {
 //interaction between JavaScript and python file
 function data_bus(dataLabel, converter) {
 
-    //If no data in converter, get Data user entered
+    //if no data in converter, send data to user
     if (!converter) {
-        $.ajax({
-            url: "",
-            type: "GET",
-            contentType: dataLabel,
-            data: JSON.stringify(converter)
-        });
+        //need server url? trying movie server
+        const myRequest = new Request("movies.json");
+        fetch(myRequest)
+            .then((response) => response.json())
+            .then((return_data) => {
+
+                //for every movie in the json file
+                for (const movies of return_data.movies) {
+
+                    //create a new list item
+                    const list_item = document.createElement("li");
+
+                    list_item.appendChild(document.createElement("p")).textContent = movies.title;
+                    list_item.append(` can be found at theater ${movies.theater} located at ${movies.location}.`);
+                }
+            })
+
+            .catch(console.error);
     } else {
 
-        //Else send Data to user
+        //else send data to url for py script to listen
         $.ajax({
+            //need url for python to listen on
             url: "",
             type: "POST",
-            contentType: dataLabel,
+            contentType: "application/json",
             data: JSON.stringify(converter)
         });
 
